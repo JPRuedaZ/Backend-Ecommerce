c81acd7ccdd06983c47a39e1bb68e54ba9dcf080
JPRuedaZ <juanpabloruedazo@gmail.com>
2024-06-07 09:50:10 -0500

    ajustando el cors para el front

--- back/ecommerce-jprueda-z/src/main.ts
+++ back/ecommerce-jprueda-z/src/main.ts
@@ -19,6 +19,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,PUT,POST,DELETE', 
+    allowedHeaders: 'Content-Type',
+  });
   await app.listen(3000);
 }
 bootstrap();
